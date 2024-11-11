5291fca7a54709aa0d1737b9a12ed626c21aec27
jhartlat <jhart.lat@gmail.com>
2024-11-11 09:38:59 -0600

    add function to print report

--- main.py
+++ main.py
@@ -10,15 +10,30 @@ def count_characters(text):
 
     return char_count
 
+
+def print_report(filepath, char_count):
+
+    print(f'--- Begin report of {filepath} ---')
+
+    words = char_count[' ']
+    print(f'{words:,} words found in the document' + '\n')
+
+    a_z = 'abcdefghijklmnopqrstuvwxyz'
+    for char in a_z:
+        if char in char_count:
+            print(f'The letter \'{char}\' was found {char_count[char]:,} times')
+
+    print('--- End Report ---')
+
+
 def main():
 
-    with open('books/frankenstein.txt') as book:
-        book_contents = book.read()
+    filepath = 'books/frankenstein.txt'
+    with open(filepath) as file:
+        text = file.read()
 
-
-    char_count = count_characters(book_contents)
-    for char in char_count:
-        print(f'{char}: {char_count[char]}')
+    char_count = count_characters(text)
+    print_report(filepath, char_count)
 
 
 main()
